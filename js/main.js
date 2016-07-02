afe730b042d7f0c7c216417641db2d05441bffae
Jason Bejot <jason.t.bejot@disney.com>
2016-07-02 12:51:23 -0700

    goodreads tweaks

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(function(){
 
-  $.get({
+  /*$.get({
     url:'https://www.goodreads.com/review/list?v=2&id=37984300&key=bMQakmDgjvTY0R6mtWaBBg',
     success: function(){
       console.log(arguments);
     }
-  })
+  })*/
 });
