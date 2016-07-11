28aef1b79f7c4101379f00ada75b111e6b8955a4
skullnbones <jasonbejot@gmail.com>
2016-07-10 20:18:46 -0700

    implemented personal api for goodreads

--- js/main.js
+++ js/main.js
@@ -1,9 +1,47 @@
-$(function(){
+var api = {
+  goodreads: {},
+  medium: {}
+};
 
-  /*$.get({
-    url:'https://www.goodreads.com/review/list?v=2&id=37984300&key=bMQakmDgjvTY0R6mtWaBBg',
-    success: function(){
-      console.log(arguments);
+api.goodreads.getCurrentBook = function(success, error){
+  return $.get({
+    url: 'https://api-skullnbones.c9users.io/goodreads/current',
+    success: success,
+    error: function(e){
+      console.error(e);
+      error.apply(this, arguments);
     }
-  })*/
+  });
+};
+
+api.goodreads.getReadBooks = function(success, error){
+  return $.get({
+    url: 'https://api-skullnbones.c9users.io/goodreads/read',
+    success: success,
+    error: function(e){
+      console.error(e);
+      error.apply(this, arguments);
+    }
+  });
+};
+
+api.medium.getPosts = function(success, error){
+  return $.get({
+    url: 'https://api-skullnbones.c9users.io/medium/posts',
+    success: success,
+    error: function(e){
+      console.error(e);
+      error.apply(this, arguments);
+    }
+  });
+};
+
+$(function(){
+  api.goodreads.getReadBooks(function(data){
+    $('#books .read .books').append(data);
+  });
+
+  api.goodreads.getCurrentBook(function(data){
+    $('#books .current').append(data);
+  })
 });
