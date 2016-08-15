6387203d5ef616a1d986dd5f6284ae38eb51fe19
corpsecouch <jasonbejot@gmail.com>
2016-08-14 19:47:26 -0700

    removed experimental require.js shims

--- js/app.js
+++ js/app.js
@@ -3,22 +3,7 @@ requirejs.config({
   paths: {
     'jquery'          : '../bower_components/jquery/dist/jquery.min',
     'jquery-ui'       : '../bower_components/jquery-ui/jquery-ui.min'
-  },
-  /*shim: {
-    'CopyShader': 'threejs',
-    'EffectComposer': 'threejs',
-    'ShaderPass': 'threejs',
-    'RenderPass': 'threejs',
-    'GlitchPass': ['threejs', 'shaders/DigitalGlitch'],
-    'EdgeShader': 'threejs',
-    'FilmPass': ['threejs', 'FilmShader'],
-    'MirrorShader': 'threejs',
-    'BloomPass': ['threejs', 'ConvolutionShader'],
-    'TriangleBlurShader': 'threejs',
-    'VerticalBlurShader': 'threejs',
-    'BokehPass': ['threejs', 'BokehShader'],
-    'FXAAShader': 'threejs',
-  }*/
+  }
 });
 
 requirejs(['main']);
