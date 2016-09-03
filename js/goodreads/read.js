4a391298bde0e7e2aee47869bcada1c31cd1f729
corpsecouch <jasonbejot@gmail.com>
2016-09-02 17:10:12 -0700

    shuffling the goodreads read books

--- js/goodreads/read.js
+++ js/goodreads/read.js
@@ -17,10 +17,25 @@ define(['jquery-ui'], function(){
         console.error();
       })
       .done((data) => {
+        // shuffle the books
+        this.shuffle(data.data);
+
+        // display the books
         data.data.forEach(function(val){
           $(this.element).append(`<a title="${val.title}" href="${val.href}"><img src="${val.src}" alt="${val.title}"></a>`);
         }, this);
       });
+    },
+
+    // randomize an array
+    shuffle: function(a){
+      var j, x, i;
+      for (i = a.length; i; i--) {
+          j = Math.floor(Math.random() * i);
+          x = a[i - 1];
+          a[i - 1] = a[j];
+          a[j] = x;
+      }
     }
 
   })
