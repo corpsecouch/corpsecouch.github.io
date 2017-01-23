ce6cf46bb6571610c0a632f7d620f19ad42a3697
corpsecouch <jasonbejot@gmail.com>
2017-01-22 22:27:05 -0800

    removed old static stuff

--- js/app.js
+++
@@ -1,9 +0,0 @@
-requirejs.config({
-  baseUrl: 'js',
-  paths: {
-    'jquery'          : '../bower_components/jquery/dist/jquery.min',
-    'jquery-ui'       : '../bower_components/jquery-ui/jquery-ui.min'
-  }
-});
-
-requirejs(['main']);
