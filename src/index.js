a592dd6546bfaba7494a4e76df2a7eb3feae110a
corpsecouch <jasonbejot@gmail.com>
2019-01-25 22:28:16 -0800

    updated the 404 to vue routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import _ from 'lodash';
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import Index from 'components/Index';
-import VueResource from 'vue-resource'
-Vue.use(VueResource);
+import FourOhFour from 'components/FourOhFour';
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/', component: Index },
+  { path: '/404', component: FourOhFour },
+  { path: '*', redirect: '/404' }
+];
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
 
 new Vue({
   el: '#app',
-  components: {
-    Index
-  }
+  router
 });
