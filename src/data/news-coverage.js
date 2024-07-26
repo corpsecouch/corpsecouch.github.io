b4dee2d4737461a5db533320237d2f4c755e5428
Jason Bejot <jasonbejot@gmail.com>
2024-07-26 11:14:36 -0400

    Adjusted the news data

--- src/data/news-coverage.js
+++ src/data/news-coverage.js
@@ -2,177 +2,226 @@ const data = [
   {
     pub:    'National Mortgage News',
     title:  'Rocket Pro TPO teases AI-powered hotline',
-    url:    'https://www.nationalmortgagenews.com/news/rocket-pro-tpo-teases-ai-powered-hotline'
+    url:    'https://www.nationalmortgagenews.com/news/rocket-pro-tpo-teases-ai-powered-hotline',
+    project:'',
+    year:   '',
+    company:'Rocket'
   },
 
   {
     pub:    'National Mortgage Professional',
     title:  'Rocket Pro TPO Introduces Hotline For Brokers',
-    url:    'https://nationalmortgageprofessional.com/news/rocket-pro-tpo-introduces-hotline-brokers'
+    url:    'https://nationalmortgageprofessional.com/news/rocket-pro-tpo-introduces-hotline-brokers',
+    project:'',
+    year:   '',
+    company:'Rocket'
   },
 
   {
     pub:    'The Mortgage Note',
     title:  'Rocket Pro TPO Announces AI-Powered Hotline For Brokers',
-    url:    'https://www.themortgagenote.org/rocket-pro-tpo-announces-hotline-for-brokers-powered-by-ai/'
+    url:    'https://www.themortgagenote.org/rocket-pro-tpo-announces-hotline-for-brokers-powered-by-ai/',
+    project:'',
+    year:   '',
+    company:'Rocket'
   },
 
   {
     pub:    'ScotsmanGuide',
     title:  'Industry Watch: A new AI hotline at Rocket',
-    url:    'https://www.scotsmanguide.com/news/industry-watch-a-new-ai-hotline-at-rocket-a-program-for-real-estate-agents-at-guaranteed-rate-and-more/'
+    url:    'https://www.scotsmanguide.com/news/industry-watch-a-new-ai-hotline-at-rocket-a-program-for-real-estate-agents-at-guaranteed-rate-and-more/',
+    project:'',
+    year:   '',
+    company:'Rocket'
   },
   
   {
     pub:    'The New York Times',
     title:  '‘Cortana, Open Alexa,’ Amazon Says. And Microsoft Agrees.',
     url:    'https://www.nytimes.com/2017/08/30/technology/amazon-alexa-microsoft-cortana.html',
-    project:'AmazonAlexaCortana'
+    project:'AmazonAlexaCortana',
+    year:   '',
+    company:'Amazon'
   },
   
   {
     pub:    'Gizmodo',
     title:  'Alexa and Cortana Are on Speaking Terms',
     url:    'https://gizmodo.com/alexa-and-cortana-are-on-speaking-terms-1798622487',
-    project:'AmazonAlexaCortana'
+    project:'AmazonAlexaCortana',
+    year:   '',
+    company:'Amazon'
   },
   
   {
     pub:    'The Verge',
     title:  'Microsoft and Amazon partner to integrate Alexa and Cortana digital assistants',
     url:    'https://www.theverge.com/2017/8/30/16224876/microsoft-amazon-cortana-alexa-partnership',
-    project:'AmazonAlexaCortana'
+    project:'AmazonAlexaCortana',
+    year:   '',
+    company:'Amazon'
   },
   
   {
     pub:    'The Verge',
     title:  'Microsoft demonstrates Alexa and Cortana integration',
     url:    'https://www.theverge.com/2018/5/7/17326808/microsoft-cortana-update-alexa-integration-build-2018',
-    project:'AmazonAlexaCortana'
+    project:'AmazonAlexaCortana',
+    year:   '',
+    company:'Amazon'
   },
   
   {
     pub:    'cnet',
     title:  'How to use Alexa and Cortana together',
     url:    'https://www.cnet.com/how-to/how-to-use-alexa-and-cortana-together/',
-    project:'AmazonAlexaCortana'
+    project:'AmazonAlexaCortana',
+    year:   '',
+    company:'Amazon'
   },
   
   {
     pub:    'ZDnet',
     title:  'The Alexa-Cortana integration: Why wasn\'t this bigger news?',
     url:    'https://www.zdnet.com/article/the-alexa-cortana-integration-why-wasnt-this-bigger-news/',
-    project:'AmazonAlexaCortana'
+    project:'AmazonAlexaCortana',
+    year:   '',
+    company:'Amazon'
   },
   
   {
     pub:    'Lifehacker',
     title:  'How to Use Cortana and Alexa together',
     url:    'https://lifehacker.com/how-to-use-cortana-and-alexa-together-1828386070',
-    project:'AmazonAlexaCortana'
+    project:'AmazonAlexaCortana',
+    year:   '',
+    company:'Amazon'
   },
   
   {
     pub:    'TechCrunch',
     title:  'The Alexa-Cortana integration is now available in a public preview',
     url:    'https://techcrunch.com/2018/08/15/the-alexa-cortana-integration-is-now-available-in-a-public-preview/',
-    project:'AmazonAlexaCortana'
+    project:'AmazonAlexaCortana',
+    year:   '',
+    company:'Amazon'
   },
 
   {
     pub:    'VentureBeat',
     title:  'Microsoft and Amazon launch Alexa-Cortana public preview for Echo speakers and Windows 10 PCs',
     url:    'https://venturebeat.com/ai/microsoft-and-amazon-launch-alexa-cortana-public-preview-for-echo-speakers-and-windows-10-pcs/',
-    project:'AmazonAlexaCortana'
+    project:'AmazonAlexaCortana',
+    year:   '',
+    company:'Amazon'
   },
 
   {
     pub:    'VentureBeat',
     title:  'Alexa meets Cortana: Amazon and Microsoft to integrate their digital assistants',
     url:    'https://venturebeat.com/ai/alexa-meets-cortana-microsoft-and-amazon-to-integrate-their-digital-assistants/',
-    project:'AmazonAlexaCortana'
+    project:'AmazonAlexaCortana',
+    year:   '',
+    company:'Amazon'
   },
 
   {
     pub:    'ars technica',
     title:  'Alexa and Cortana integration starts rolling out today',
     url:    'https://arstechnica.com/gadgets/2018/08/alexa-and-cortana-integration-starts-rolling-out-today/',
-    project:'AmazonAlexaCortana'
+    project:'AmazonAlexaCortana',
+    year:   '',
+    company:'Amazon'
   },
 
   {
     pub:    'ars technica',
     title:  'Cortana and Alexa are coming together in surprising Microsoft-Amazon partnership',
     url:    'https://arstechnica.com/gadgets/2017/08/cortana-and-alexa-are-coming-together-in-surprising-microsoftamazon-partnership/',
-    project:'AmazonAlexaCortana'
+    project:'AmazonAlexaCortana',
+    year:   '',
+    company:'Amazon'
   },
 
   {
     pub:    'PC Mag',
     title:  'Alexa, Open Cortana: Amazon, Microsoft Team Up on Voice',
     url:    'https://www.pcmag.com/news/alexa-open-cortana-amazon-microsoft-team-up-on-voice',
-    project:'AmazonAlexaCortana'
+    project:'AmazonAlexaCortana',
+    year:   '',
+    company:'Amazon'
   },
 
   {
     pub:    'PC Mag',
     title:  'Alexa and Cortana Become Smart Friends',
     url:    'https://www.pcmag.com/news/alexa-and-cortana-become-smart-friends',
-    project:'AmazonAlexaCortana'
+    project:'AmazonAlexaCortana',
+    year:   '',
+    company:'Amazon'
   },
 
   {
     pub:    'Slate',
     title:  'Alexa Is Coming for Your Laptop',
     url:    'https://slate.com/technology/2018/06/alexa-amazon-assistant-moving-to-your-laptop-does-it-make-sense.html',
-    project:'AmazonAlexaCortana'
-  },
-
-  {
-    pub:    'Slate',
-    title:  'Alexa Is Coming for Your Laptop',
-    url:    'https://slate.com/technology/2018/06/alexa-amazon-assistant-moving-to-your-laptop-does-it-make-sense.html',
-    project:'AmazonAlexaCortana'
+    project:'AmazonAlexaCortana',
+    year:   '',
+    company:'Amazon'
   },
 
   {
     pub:    'Wired',
     title:  'An Alexa and Cortana Team-Up Won\'t Get Far Without a Phone',
     url:    'https://www.wired.com/story/alexa-cortana-team-up-wont-get-far-without-a-phone/',
-    project:'AmazonAlexaCortana'
+    project:'AmazonAlexaCortana',
+    year:   '',
+    company:'Amazon'
   },
 
   {
     pub:    'CNBC',
     title:  'Amazon and Microsoft team up their A.I. voice assistants with new partnership deal',
     url:    'https://www.cnbc.com/2017/08/30/amazon-digital-voice-assistant-alexa-partners-with-microsoft-cortana.html',
-    project:'AmazonAlexaCortana'
+    project:'AmazonAlexaCortana',
+    year:   '',
+    company:'Amazon'
   },
 
   {
     pub:    'The Washington Post',
     title:  'Microsoft and Amazon made their voice assistants into friends. Here\'s how that relationship works.',
     url:    'https://www.washingtonpost.com/technology/2018/08/16/microsoft-amazon-made-their-voice-assistants-into-friends-heres-how-that-relationship-works/',
-    project:'AmazonAlexaCortana'
+    project:'AmazonAlexaCortana',
+    year:   '',
+    company:'Amazon'
   },
 
   {
     pub:    'TechRadar',
     title:  'Here\'s what Alexa and Cortana Windows 10 integration looks like',
     url:    'https://www.techradar.com/news/heres-what-alexa-and-cortana-windows-10-integration-looks-like',
-    project:'AmazonAlexaCortana'
+    project:'AmazonAlexaCortana',
+    year:   '',
+    company:'Amazon'
   },
 
   {
     pub:    'Amazon',
     title:  'Alexa became even more natural and useful for customers in 2019',
-    url:    'https://www.aboutamazon.com/news/devices/alexa-became-even-more-natural-and-useful-for-customers-in-2019'
+    url:    'https://www.aboutamazon.com/news/devices/alexa-became-even-more-natural-and-useful-for-customers-in-2019',
+    project:'',
+    year:   '',
+    company:'Amazon'
   },
 
   {
     pub:    'voicebot.ai',
     title:  'Amazon Launches Alexa Skill Personalization Features',
-    url:    'https://voicebot.ai/2019/11/01/amazon-launches-alexa-skill-personalization-features/'
+    url:    'https://voicebot.ai/2019/11/01/amazon-launches-alexa-skill-personalization-features/',
+    project:'',
+    year:   '',
+    company:'Amazon'
   }
 
 ]
