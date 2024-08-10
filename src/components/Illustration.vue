7b42a4da9c55ea218de3b5d0019cd270929dd35d
Jason Bejot <jasonbejot@gmail.com>
2024-08-10 15:49:51 -0400

    added images to the alexa profle and alexa + cortana case studies. also created an illustration component to consistently style the images

---
+++ src/components/Illustration.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="illustration">
+    <img :src="imagesrc">
+    <p class="subtext" v-if="!!this.$slots.subtext"><slot name="subtext"></slot></p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Illustration',
+    props: [ 'imagesrc' ]
+  }
+</script>
+
+<style scoped lang="scss">
+  .illustration {
+    margin-bottom: 3rem;
+  }
+
+  img {
+    width: 100%;
+    margin-top: 1rem;
+  }
+
+  .subtext {
+    color: grey;
+    font-size: 0.8rem;
+    font-style: italic;
+    text-align: center;
+    margin-top: 0.5rem;
+  }
+</style>
--- src/pages/portfolio/alexa/alexa-cortana/alexa-cortana.vue
+++ src/pages/portfolio/alexa/alexa-cortana/alexa-cortana.vue
@@ -16,12 +16,21 @@
       <p>This was more than just connecting APIs or bringing Alexa's jokes and shopping to Cortana. This was about enabling two competitive AIs to work together for the benefit of the end-user. There were four key challenges I needed to overcome to figure this out:</p>
       <h4>Vision</h4>
       <p>This was going to be the beginning of a new modality for Alexa and a new, innovative line of business. I needed to craft a compelling vision for a program, not just a product.</p>
+      <Illustration :imagesrc="require(`./image1.jpg`)">
+        <template v-slot:subtext>Peeking around the corner of what interoperable A.I.'s could unlock.</template>
+      </Illustration>
       <h4>AI Orchestration</h4>
       <p>How do two AIs work together? How do you know what to do when you switch between them? How do you not lose track of which one you're talking to? These are fundamental questions. I needed to design how people interact with multiple AIs through the same interface.</p>
+      <Illustration :imagesrc="require(`./image3.jpg`)">
+        <template v-slot:subtext>A peek at the extensive user testing.</template>
+      </Illustration>
       <h4>Scale</h4>
       <p>Each AI assitant has it's own modalities, technical restrictions, and design systems. I needed a way to translate existing experiences from one AI to the other and do it at scale.</p>
       <h4>Holistic Approach</h4>
       <p>Each AI assistant is intrinsically tied to its hardware. There was a large tech stack gap across the hardware and software between the AIs. I needed to look holistally across both hardware and software to create a successful product and program.</p>
+      <Illustration :imagesrc="require(`./image2.jpg`)">
+        <template v-slot:subtext>A peek at the blueprints of a multi-agent experience.</template>
+      </Illustration>
     </template>
 
     <template v-slot:outcome>
@@ -45,6 +54,7 @@
   import YouTubeVideo from 'components/YouTubeVideo';
   import newsData from 'data/news-coverage';
   import NewsList from 'components/NewsList';
+  import Illustration from 'components/Illustration';
 
   export default {
     name: 'AmazonAlexaCortana',
@@ -52,7 +62,8 @@
     components: {
       PortfolioPage,
       YouTubeVideo,
-      NewsList
+      NewsList,
+      Illustration
     },
 
     data() {
