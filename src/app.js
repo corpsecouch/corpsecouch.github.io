d7308388b5fa6c471af980c5c66d10f805b466ee
Jason Bejot <jasonbejot@gmail.com>
2024-10-02 17:09:46 -0400

    cleaned up some of the router code

--- src/app.js
+++ src/app.js
@@ -15,69 +15,28 @@ const routes = [
   { name: 'index',                        component: () => import('pages/Index') },
   { name: 'press',                        component: () => import('pages/Press') },
   { name: 'portfolio',                    component: () => import('pages/Portfolio') },
-
-  { name:'rocketironbear',                component: () => import('pages/portfolio/rocket/ironbear/ironbear') },
-
-  { name:'alexacortana',                  component: () => import('pages/portfolio/alexa/alexa-cortana/alexa-cortana') },
-  { name:'alexaprofile',                  component: () => import('pages/portfolio/alexa/profile/profile') },
-
-  { name:'disneydesignsystem',            component: () => import('pages/portfolio/disney/design-system/design-system') },
-  { name:'disneygreenlight',              component: () => import('pages/portfolio/disney/greenlight/greenlight') },
-  { name:'disneyinnovationprogram',       component: () => import('pages/portfolio/disney/incubator/incubator') },
-  { name:'disneymovieslate',              component: () => import('pages/portfolio/disney/slate/slate') },
-  { name:'disneyvr',                      component: () => import('pages/portfolio/disney/vr/vr') },
-
-  { name:'pbgenesis',                     component: () => import('pages/portfolio/phenomblue/genesis/genesis') },
-  { name:'pbdinodig',                     component: () => import('pages/portfolio/phenomblue/dino-dig/dino-dig') },
-  { name:'pbmovielotto',                  component: () => import('pages/portfolio/phenomblue/movie-lotto/movie-lotto') },
-  { name:'pbfantasticfutureme',           component: () => import('pages/portfolio/phenomblue/fantastic-future-me/fantastic-future-me') },
-  { name:'pbmoppetmashup',                component: () => import('pages/portfolio/phenomblue/moppet-mashup/moppet-mashup') },
-  { name:'pbwildkingdom',                 component: () => import('pages/portfolio/phenomblue/wild-kingdom/wild-kingdom') },
-  { name:'pbpage',                        component: () => import('pages/portfolio/phenomblue/page/page') },
-  { name:'pbtums',                        component: () => import('pages/portfolio/phenomblue/tums/tums') },
-
+  { name: 'rocketironbear',                component: () => import('pages/portfolio/rocket/ironbear/ironbear') },
+  { name: 'alexacortana',                  component: () => import('pages/portfolio/alexa/alexa-cortana/alexa-cortana') },
+  { name: 'alexaprofile',                  component: () => import('pages/portfolio/alexa/profile/profile') },
+  { name: 'disneydesignsystem',            component: () => import('pages/portfolio/disney/design-system/design-system') },
+  { name: 'disneygreenlight',              component: () => import('pages/portfolio/disney/greenlight/greenlight') },
+  { name: 'disneyinnovationprogram',       component: () => import('pages/portfolio/disney/incubator/incubator') },
+  { name: 'disneymovieslate',              component: () => import('pages/portfolio/disney/slate/slate') },
+  { name: 'disneyvr',                      component: () => import('pages/portfolio/disney/vr/vr') },
+  { name: 'pbgenesis',                     component: () => import('pages/portfolio/phenomblue/genesis/genesis') },
+  { name: 'pbdinodig',                     component: () => import('pages/portfolio/phenomblue/dino-dig/dino-dig') },
+  { name: 'pbmovielotto',                  component: () => import('pages/portfolio/phenomblue/movie-lotto/movie-lotto') },
+  { name: 'pbfantasticfutureme',           component: () => import('pages/portfolio/phenomblue/fantastic-future-me/fantastic-future-me') },
+  { name: 'pbmoppetmashup',                component: () => import('pages/portfolio/phenomblue/moppet-mashup/moppet-mashup') },
+  { name: 'pbwildkingdom',                 component: () => import('pages/portfolio/phenomblue/wild-kingdom/wild-kingdom') },
+  { name: 'pbpage',                        component: () => import('pages/portfolio/phenomblue/page/page') },
+  { name: 'pbtums',                        component: () => import('pages/portfolio/phenomblue/tums/tums') },
   { name: '404',                          component: () => import('pages/FourOhFour') },
   { path: '/:pathMatch(.*)*',             redirect: '/404' }
 ]
 
 import routesData from 'data/routes';
 _.merge(routes, routesData);
-
-/*const routes = [
-  { path: '/',                                          name: 'index',                        component: () => import('pages/Index') },
-  { path: '/press',                                     name: 'press',                        component: () => import('pages/Press') },
-  { path: '/portfolio',                                 name: 'portfolio',                    component: () => import('pages/Portfolio') },
-  
-  //{ path: '/#about',                                    name: 'about',                        component: Index },
-  //{ path: '/#contact',                                  name: 'contact',                      component: Index },
-
-  { path: '/portfolio/rocket/ironbear',                 name:'rocketironbear',                component: () => import('pages/portfolio/rocket/ironbear/ironbear') },
-  //{ path: '/portfolio/rocket/project-two',              name:'rocketprojecttwo',                   component: () => import('pages/portfolio/rocket/project-two/project-two') },
-
-  { path: '/portfolio/amazon/alexa-cortana',            name:'alexacortana',                  component: () => import('pages/portfolio/alexa/alexa-cortana/alexa-cortana') },
-  //{ path: '/portfolio/amazon/presence',                 name:'alexapresence',                 component: () => import('pages/portfolio/alexa/presence/presence') },
-  //{ path: '/portfolio/amazon/personalized-skills',      name:'alexapersonalizedskills',       component: () => import('pages/portfolio/alexa/personalized-skills/personalized-skills') },
-  { path: '/portfolio/amazon/profile',                  name:'alexaprofile',                  component: () => import('pages/portfolio/alexa/profile/profile') },
-  //{ path: '/portfolio/amazon/voice-settings',           name:'alexavoicesettings',            component: () => import('pages/portfolio/alexa/voice-settings/voice-settings') },
-
-  { path: '/portfolio/disney/design-system',            name:'disneydesignsystem',            component: () => import('pages/portfolio/disney/design-system/design-system') },
-  { path: '/portfolio/disney/greenlight',               name:'disneygreenlight',              component: () => import('pages/portfolio/disney/greenlight/greenlight') },
-  { path: '/portfolio/disney/innovation-program',       name:'disneyinnovationprogram',       component: () => import('pages/portfolio/disney/incubator/incubator') },
-  { path: '/portfolio/disney/movie-slate',              name:'disneymovieslate',              component: () => import('pages/portfolio/disney/slate/slate') },
-  { path: '/portfolio/disney/vr',                       name:'disneyvr',                      component: () => import('pages/portfolio/disney/vr/vr') },
-
-  { path: '/portfolio/phenomblue/genesis',              name:'pbgenesis',                     component: () => import('pages/portfolio/phenomblue/genesis/genesis') },
-  { path: '/portfolio/phenomblue/dino-dig',             name:'pbdinodig',                     component: () => import('pages/portfolio/phenomblue/dino-dig/dino-dig') },
-  { path: '/portfolio/phenomblue/movie-lotto',          name:'pbmovielotto',                  component: () => import('pages/portfolio/phenomblue/movie-lotto/movie-lotto') },
-  { path: '/portfolio/phenomblue/fantastic-future-me',  name:'pbfantasticfutureme',           component: () => import('pages/portfolio/phenomblue/fantastic-future-me/fantastic-future-me') },
-  { path: '/portfolio/phenomblue/moppet-mashup',        name:'pbmoppetmashup',                component: () => import('pages/portfolio/phenomblue/moppet-mashup/moppet-mashup') },
-  { path: '/portfolio/phenomblue/my-wild-kingdom',      name:'pbwildkingdom',                 component: () => import('pages/portfolio/phenomblue/wild-kingdom/wild-kingdom') },
-  { path: '/portfolio/phenomblue/page',                 name:'pbpage',                        component: () => import('pages/portfolio/phenomblue/page/page') },
-  { path: '/portfolio/phenomblue/tums',                 name:'pbtums',                        component: () => import('pages/portfolio/phenomblue/tums/tums') },
-
-  { path: '/404',                                       name: '404',                          component: () => import('pages/FourOhFour') },
-  { path: '/:pathMatch(.*)*',                           redirect: '/404' }
-];*/
 
 import { createRouter, createWebHashHistory } from 'vue-router';
 
