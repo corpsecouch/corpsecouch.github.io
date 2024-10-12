912891987eaef352eee9a881dc72cb671e6ed327
Jason Bejot <jasonbejot@gmail.com>
2024-10-12 07:58:27 -0400

    removed log from vars, added page title and url to gtag page tracking

--- globals/vars.js
+++ globals/vars.js
@@ -2,6 +2,4 @@
 const _isProd = process.env.NODE_ENV === 'production';
 const _GtagID = 'G-G24FHEZ8YC';
 
-console.log("_isProd:", _isProd);
-
 export {_isProd, _GtagID};
--- portfolio/phenomblue-wild-kingdom/phenomblue-wild-kingdom.js
+++ portfolio/phenomblue-wild-kingdom/phenomblue-wild-kingdom.js
@@ -5,6 +5,12 @@ import {_isProd, _GtagID } from '@globals/vars'
 
 createApp(App)
 .use(VueGtag, {
+    pageTrackerTemplate(to) {
+        return {
+            page_title: document.title,
+            page_path: to.path
+        }
+    },
     config: { id: _GtagID },
     enabled: _isProd
 })
