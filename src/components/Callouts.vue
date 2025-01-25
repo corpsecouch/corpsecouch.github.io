01a112d7f88cb6ec0cd41c5dffe06850029ffe2e
Jason Bejot <jasonbejot@gmail.com>
2025-01-25 11:20:22 -0500

    converted the callouts component to use module styles instead of scoped

--- src/components/Callouts.vue
+++ src/components/Callouts.vue
@@ -1,40 +1,16 @@
 <template>
 
-  <section id="callouts" v-if="pressTotal || awardsTotal">
+  <section :class="$style.callouts" v-if="pressTotal || awardsTotal">
     
-    <div class="press" v-if="pressTotal">
-      <span class="count">{{ pressTotal }}</span>
-      <a class="description" :href="pressURL">Article{{ pressTotal > 1 ? 's' : '' }}</a>
+    <div :class="$style.press" v-if="pressTotal">
+      <span :class="$style.count">{{ pressTotal }}</span>
+      <a :class="$style.description" :href="pressURL">Article{{ pressTotal > 1 ? 's' : '' }}</a>
     </div>
     
-    <div class="awards" v-if="awardsTotal">
-      <span class="count">{{ awardsTotal }}</span>
-      <a class="description" :href="awardsURL">Award{{ awardsTotal > 1 ? 's' : '' }}</a>
+    <div :class="$style.awards" v-if="awardsTotal">
+      <span :class="$style.count">{{ awardsTotal }}</span>
+      <a :class="$style.description" :href="awardsURL">Award{{ awardsTotal > 1 ? 's' : '' }}</a>
     </div>
-
-    <!-- <div class="press" v-if="pressTotal">
-      <div class="content">
-        <div class="left">
-          <div class="icon"><SVGNews /></div>
-        </div>
-        <div class="right">
-          <span class="count">{{ pressTotal }}</span>
-          <a class="description" :href="pressURL">Article{{ pressTotal > 1 ? 's' : '' }}</a>
-        </div>
-      </div>
-    </div>
-    
-    <div class="awards" v-if="awardsTotal">
-      <div class="content">
-        <div class="left">
-          <div class="icon"><SVGAwardOriginal /></div>
-        </div>
-        <div class="right">
-          <span class="count">{{ awardsTotal }}</span>
-          <span class="description">Award{{ awardsTotal > 1 ? 's' : '' }}</span>
-        </div>
-      </div>
-    </div> -->
 
   </section>
 
@@ -43,8 +19,6 @@
 <script>
   export default {
     name: 'Callouts',
-
-    components: {},
 
     props: [
       'pressTotal',
@@ -55,8 +29,8 @@
   }
 </script>
 
-<style scoped lang="css">
-  #callouts {
+<style module lang="css">
+  .callouts {
     margin-top: 1.3rem;
     display: flex;
     margin-bottom: 4rem;
