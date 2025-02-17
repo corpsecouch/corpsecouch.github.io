58b818d542521f8500c2456ddeeaf458f61ef1e3
Jason Bejot <jasonbejot@gmail.com>
2025-02-17 09:15:40 -0500

    cleaned up commented-out styles

--- src/.vitepress/theme/page.vue
+++ src/.vitepress/theme/page.vue
@@ -14,25 +14,11 @@
 </script>
 
 <style module lang="css">
-  /* #page {
-    padding-top: 6rem;
-  } */
-
   .title {
-    /* font-family: 'Mainstay';
-    letter-spacing: normal;
-    font-size: 5rem;
-    font-weight: normal;
-    line-height: 4.5rem;
-    text-transform: none; */
-
     font-family: 'Afacad Flux';
     letter-spacing: 0.1em;
     font-weight: 400;
-
     text-align: left;
-    /* font-weight: 400; */
-    /* letter-spacing: normal; */
     font-size: 2.8rem;
     text-transform: none;
     margin: 0 0 1em 0;
